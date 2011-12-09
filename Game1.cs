f53068eb5d34068b194c683e977fb37004dca731
Nathan DeGrand <degrand1@illinois.edu>
2011-12-08 20:48:06 -0600

    Hacked together some code to make a light that reveals a picture in the center

--- Game1.cs
+++ Game1.cs
@@ -19,6 +19,21 @@ namespace particleFun
         SpriteBatch spriteBatch;
         ParticleEngine particleEngine;
 
+        private Texture2D blue;
+        private Texture2D green;
+        private Texture2D red;
+        private Texture2D sonic;
+
+        private float blueAngle = 0;
+        private float greenAngle = 0;
+        private float redAngle = 0;
+
+        private float blueSpeed = 0.025f;
+        private float greenSpeed = 0.017f;
+        private float redSpeed = 0.022f;
+
+        private float distance = 50;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -38,6 +53,11 @@ namespace particleFun
             textures.Add(Content.Load<Texture2D>("circle"));
             textures.Add(Content.Load<Texture2D>("star"));
             textures.Add(Content.Load<Texture2D>("diamond"));
+
+            sonic = Content.Load<Texture2D>("sonic");
+            blue = Content.Load<Texture2D>("blue");
+            green = Content.Load<Texture2D>("green");
+            red = Content.Load<Texture2D>("red");
             particleEngine = new ParticleEngine(textures, new Vector2(400, 240));
         }
 
@@ -50,18 +70,52 @@ namespace particleFun
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
-
+            /*
             particleEngine.EmitterLocation = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
             particleEngine.Update();
+
+            blueAngle += blueSpeed;
+            greenAngle += greenSpeed;
+            redAngle += redSpeed;
+            */
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
+            /*
+            Vector2 bluePosition = new Vector2(
+                (float)Math.Cos(blueAngle) * distance,
+                (float)Math.Sin(blueAngle) * distance);
+            Vector2 greenPosition = new Vector2(
+                            (float)Math.Cos(greenAngle) * distance,
+                            (float)Math.Sin(greenAngle) * distance);
+            Vector2 redPosition = new Vector2(
+                            (float)Math.Cos(redAngle) * distance,
+                            (float)Math.Sin(redAngle) * distance);
+
+            Vector2 center = new Vector2(300, 140);
+             */
+            Vector2 Location = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
             GraphicsDevice.Clear(Color.Black);
 
-            particleEngine.Draw(spriteBatch);
+            //particleEngine.Draw(spriteBatch);
+
+            //spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive);
+
+            spriteBatch.Begin(SpriteBlendMode.Additive,
+                  SpriteSortMode.FrontToBack,
+                  SaveStateMode.None);
+            /*spriteBatch.Draw(blue, center + bluePosition, Color.White);
+            spriteBatch.Draw(green, center + greenPosition, Color.White);
+            spriteBatch.Draw(red, center + redPosition, Color.White);*/
+            spriteBatch.Draw(sonic, new Vector2(300, 140), new Color(10, 10, 10));
+            spriteBatch.Draw(blue, Location, Color.White);
+            spriteBatch.Draw(green, Location, Color.White);
+            spriteBatch.Draw(red, Location, Color.White);
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
